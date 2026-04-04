06c3d2dd1855c44397967c60e447d45bf7b2c7b3
Dzidson <pawelsierant@gmail.com>
2026-04-04 15:32:54 +0200

    data params

---
+++ sql/data/01_t_params.sql
@@ -0,0 +1,31 @@
+BEGIN
+    INSERT INTO t_params (
+        param_name,
+        param_value,
+        param_desc,
+        is_active,
+        created_at
+    ) VALUES ( 'LOG_DIRECTORY',
+               'D_ETL_LOG',
+               'Oracle DIRECTORY dla logów',
+               'Y',
+               sysdate );
+
+    INSERT INTO t_params (
+        param_name,
+        param_value,
+        param_desc,
+        is_active,
+        created_at
+    ) VALUES ( 'LOG_FILE_MASK',
+               '{LOG_TYPE}_{PROCESS_NAME}_{PROCEDURE_NAME}_{LOAD_ID}_{TS}.log',
+               'Maska pliku logów',
+               'Y',
+               sysdate );
+
+    COMMIT;
+EXCEPTION
+    WHEN dup_val_on_index THEN
+        NULL;
+END;
+/
